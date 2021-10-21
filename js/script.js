f906d38a4e8a340527d80d26f8fb655b68b1546f
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-21 14:37:41 +0200

    ciclando nell'array

--- js/script.js
+++ js/script.js
@@ -41,3 +41,19 @@ const teamContainer = document.querySelector(".team-container");
 let card = '<div class="team-card"><div class="card-image"><img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett" /></div><div class="card-text"><h3>Wayne Barnett</h3><p>Founder & CEO</p></div></div>';
 
 teamContainer.innerHTML += card;
+
+
+
+
+
+
+for (let i = 0; i < arrayTeam.length; i++) {
+    let persone = arrayTeam[i];
+    console.log(persone);
+
+
+    for (let key in persone) {
+        console.log(key);
+    }
+}
+
